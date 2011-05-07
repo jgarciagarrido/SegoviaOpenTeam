d1e789ec530ae6f9b2bf67552a9c55ab392ccd12
Diego Cebrián <dcebrian@serincas.com>
2011-05-07 16:26:18 +0200

    Añadida clase ManejadorFichero y borrados ficheros duplicados y .pyc

--- src/quemepongo/cargadatos/models.py
+++ src/quemepongo/cargadatos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import pygrib
 
 # Create your models here.
+
+class ManejadorFichero(object):
+    ruta = ''
+    fichero = None
+    
+    def __init__(self, ruta = '/'):
+        self.ruta = ruta
+    
+    def abrir(self):
+        self.fichero = pygrib.open(self.ruta)
+    
+    def leer_temperatura(self, x, y):
+        temperaturas = self.fichero[2]
+        valores = temperaturas.values
+        latitudes, longitudes = temperaturas.latlons()
+        return { 'temperatura': valores[x][y], 'latitud': latitudes[x][y], 'longitud': longitudes[x][y]}
+    
--- src/quemepongo/cargadatos/tests.py
+++ src/quemepongo/cargadatos/tests.py
@@ -5,12 +5,36 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.utils import unittest
+from quemepongo.cargadatos.models import ManejadorFichero
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class PruebaLecturaArchivo(unittest.TestCase):
+    
+    ruta = '/tmp/fichero'
+    def setUp(self):
+        self.Archivo = ManejadorFichero(self.ruta)
+
+    def test_comprobar_instancia(self):
+        self.assertIsInstance(self.Archivo, ManejadorFichero)
+    
+    def test_comprobar_ruta(self):
+        self.assertEqual(self.Archivo.ruta, self.ruta)
+    
+    def test_abrir_fichero(self):
+        self.Archivo.abrir()
+        self.assertIsNotNone(self.Archivo.fichero)
+    
+    def test_comprobar_temperatura(self):
+        self.Archivo.abrir()
+        temperatura_posicion = self.Archivo.leer_temperatura( 0, 0)
+        self.assertIsNotNone(temperatura_posicion['temperatura'])
+        self.assertAlmostEqual(temperatura_posicion['temperatura'], 300.626235962)
+    
+    def test_comprobar_precipitacion(self):
+        self.Archivo.abrir()
+        precipitacion_posicion = self.Archivo.leer_precipitacion( 0, 0)
+        self.assertIsNotNone(precipitacion_posicion['precipitacion'])
+        self.assertAlmostEqual(precipitacion_posicion['precpitacion'], 300.626235962)
+        
+        
