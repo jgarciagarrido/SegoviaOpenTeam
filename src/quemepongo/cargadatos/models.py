d2b331798c47ce9c147503d8481cfa79fdcdffae
javiergarcia <javiergarcia@javiergarcia-HP-Compaq-nx9420-RH454ET-ABE.(none)>
2011-05-07 16:53:24 +0200

    DescargadorFichero y tests

--- src/quemepongo/cargadatos/models.py
+++ src/quemepongo/cargadatos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from ftplib import FTP
 
 # Create your models here.
+class DescargadorFichero:    
+    
+    def __init__(self, url):
+        self._url = url
+    
+    def ObtenerFichero(self, remotePath, file, localPath):
+                
+        self.ftp = FTP(self._url)
+        self.ftp.login()
+        ftpCommand = 'RETR ' + remotePath + '/' + file
+        #print ftpCommand
+        self.ftp.retrbinary(ftpCommand, open(localPath + "/" + file, 'wb').write)
+        self.ftp.quit()
+        
+    def ObtenerFicherosPrevisiones(self, remotePath, blockName, localPath):
+        
+        seq = range(3,60,6)
+        for i in seq:
+            self.ObtenerFichero(remotePath + "/" + blockName + "/", "20110507" + blockName + "0000_sfc_fc" + "%02d" % (i), localPath)
+            
--- src/quemepongo/cargadatos/tests.py
+++ src/quemepongo/cargadatos/tests.py
@@ -5,12 +5,55 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.utils import unittest
+from ftplib import FTP
+from models import DescargadorFichero
+import os.path
+import os
+import glob
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ConjuntoPruebasDescargaArchivo(unittest.TestCase):
+    
+    def setUp(self):
+        
+        fileList = glob.glob('./??????????0000_sfc_fc??')
+        for fileName in fileList:
+            os.remove(fileName)
+        
+        self.url = "ftpdatos.aemet.es"
+        self.path = "/modelos_numericos/hirlam016/"
+        self.localPath = "."
+        
+    def tearDown(self):
+        fileList = glob.glob(self.localPath + '/??????????0000_sfc_fc??')
+        for fileName in fileList:
+            os.remove(fileName)
+    
+    def testPruebaConexion(self):
+        try:
+            ftp = FTP(self.url)
+            ftp.login()
+            ftp.quit()
+        except:
+            self.fail("No se ha podido conectar a " + self.url)
+            
+    def testDescargaArchivo(self):
+        
+        fileName = '20110507000000_sfc_fc03'
+        descargador = DescargadorFichero(self.url)
+        descargador.ObtenerFichero(self.path + "/00", fileName, ".")
+        localFilePath = self.localPath + "/" + fileName
+        self.assertTrue(os.path.exists(localFilePath) and os.path.isfile(localFilePath), 'No existe el fichero ' + localFilePath)
+        
+        
+    def testDescargarPrevision(self):
+        
+        descargador = DescargadorFichero(self.url)
+        descargador.ObtenerFicherosPrevisiones(self.path, '00', self.localPath)
+        
+        fileList = glob.glob(self.localPath + '/??????????0000_sfc_fc??')
+        
+        self.assertTrue(len(fileList) == 10, "Faltan archivos")
+        
+        
+        
