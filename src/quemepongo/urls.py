98dca76e7f00417d9b38cbce76974f45b90c64b6
Javier García Garrido <javiergarcia@MacBook-Pro-de-Javier-Garcia-Garrido.local>
2011-05-08 17:05:42 +0200

    Solucionados cambios en urls.py y settings.py

--- src/quemepongo/urls.py
+++ src/quemepongo/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
-
+import os.path
+from settings import MEDIA_ROOT
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -8,6 +9,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'quemepongo.views.home', name='home'),
     # url(r'^quemepongo/', include('quemepongo.foo.urls')),
+    url(r'^recomendar/temperatura/', 'quemepongo.consejero.views.recomendar_temperatura'),
+    url(r'^recomendar/localidad/', 'quemepongo.consejero.views.recomendar_localidad'),
+    url(r'^$', 'quemepongo.consejero.views.recomendar_localidad', name='home'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -15,3 +19,4 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += patterns('django.views', (r'^images/(.*)$', 'static.serve', {'document_root': MEDIA_ROOT}), )
